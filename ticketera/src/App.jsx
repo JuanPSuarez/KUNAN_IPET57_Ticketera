7d8fe16cd8970635fa5cb277cd297c2d90064636
Zeker13S <ezequielvidela0305@gmail.com>
2023-08-30 10:11:13 -0300

    login page

---
+++ ticketera/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import login from './components/login.jsx'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    login()
+  )
+}
+
+export default App
